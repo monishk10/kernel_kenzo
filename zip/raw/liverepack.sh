05a4c8b859942df676a0a2d88ebcc06261bcf904
corphish <d97.avinaba@gmail.com>
2016-08-01 17:10:24 +0530

    liverepack: Proper copyrights
    Signed-off-by: corphish <d97.avinaba@gmail.com>

--- zip/raw/liverepack.sh
+++ zip/raw/liverepack.sh
@@ -1,4 +1,16 @@
 #!/sbin/sh
+#
+# Copyright (C) 2016 Psych Half, <psych.half@gmail.com>
+# Copyright (C) 2016 corphish, <d97.avinaba@gmail.com>
+#
+# This software is licensed under the terms of the GNU General Public
+# License version 2, as published by the Free Software Foundation, and
+# may be copied, distributed, and modified under those terms.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
 
 set -x
 
